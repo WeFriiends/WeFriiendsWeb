ae9cdec16e3ed1b11ee037386bcd39297fda4c15
Svitlana-G <svitlana.golubieva@gmail.com>
2023-01-10 17:24:25 +0100

    Added section on successful registration

--- src/components/RegistrationForm/registrationForm.js
+++ src/components/RegistrationForm/registrationForm.js
@@ -75,66 +75,75 @@ const RegistrationForm = () => {
 
     return (
         <div>
-            <div className='registrationForm'>
-                <Logo />
-                <section className="header">
-                    <h1> Enter Email</h1>
+            <Logo />
+            {success ? (
+                <section>
+                    <h1>Just one more step!</h1>
+                    <p>We sent an authentication email to </p>
+                    <p className='colorEmail'>
+                        {email}</p>
+                    <p>Please, check your mailbox.</p>
                 </section>
-                <form onSubmit={handleSubmit}>
-                    <label for="email">Login</label>
-                    <input type="email"
-                        id="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                        aria-invalid={validEmail ? "false" : "true"}
-                        aria-describedby="emailnote"
-                        onFocus={() => setEmailFocus(true)}
-                        onBlur={() => setEmailFocus(false)}
-                    ></input>
-                    <div id="emailnote" className={emailFocus && !validEmail ? "instructions" : "offscreen"} >
-                        Your Email is not correct
-                    </div>
-                    <label for="password">Password</label>
-                    <div>
-                        <input type={passwordInputType}
-                            id="password"
-                            onChange={(e) => setPwd(e.target.value)}
-                            required
-                            aria-invalid={validPwd ? "false" : "true"}
-                            aria-describedby="pwdnote"
-                            onFocus={() => setPwdFocus(true)}
-                            onBlur={() => setPwdFocus(false)}>
 
-                        </input>
-                        <span className='password-toggle-icon'>{toggleIcon}</span>
-                    </div>
-                    <div id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
-                        Your Password must have:
-                        <ul>
-                            <li className={pwd.length > 8 ? 'green' : 'rot'}> 8 or more symbols</li>
-                            <li className={/[0-9]/i.test(pwd) ? 'green' : 'rot'}>1 or more numbers</li>
-                            <li className={/[a-zA-Z]/i.test(pwd) ? 'green' : 'rot'}>1 or more Latin letters</li>
-                            <li className={/[~!@#$%^&*()_+`-]/i.test(pwd) ? 'green' : 'rot'}>1 or more special characters</li>
-                        </ul>
-                    </div>
-                    <label for="confirmPassword">One more time</label>
-                    <div>
-                        <input type={passwordInputType}
-                            id="confrimPassword"
-                            onChange={(e) => setMatchPwd(e.target.value)}
+            ) : (
+                <div className='registrationForm'>
+                    <h1> Enter Email</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label for="email">Login</label>
+                        <input type="email"
+                            id="email"
+                            onChange={(e) => setEmail(e.target.value)}
                             required
-                            aria-invalid={validMatch ? "false" : "true"}
-                            aria-describedby="confirmnote"
-                            onFocus={() => setMatchFocus(true)}
-                            onBlur={() => setMatchFocus(false)}></input>
-                        <span className='password-toggle-icon'>{toggleIcon}</span>
-                    </div>
-                    <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"} >
-                        Must match the first password input field.
-                    </p>
-                    <button className='btn' disabled={!validEmail || !validPwd || !validMatch ? true : false}>submit</button>
-                </form>
-            </div>
+                            aria-invalid={validEmail ? "false" : "true"}
+                            aria-describedby="emailnote"
+                            onFocus={() => setEmailFocus(true)}
+                            onBlur={() => setEmailFocus(false)}
+                        ></input>
+                        <div id="emailnote" className={emailFocus && !validEmail ? "instructions" : "offscreen"} >
+                            Your Email is not correct
+                        </div>
+                        <label for="password">Password</label>
+                        <div>
+                            <input type={passwordInputType}
+                                id="password"
+                                onChange={(e) => setPwd(e.target.value)}
+                                required
+                                aria-invalid={validPwd ? "false" : "true"}
+                                aria-describedby="pwdnote"
+                                onFocus={() => setPwdFocus(true)}
+                                onBlur={() => setPwdFocus(false)}>
+
+                            </input>
+                            <span className='password-toggle-icon'>{toggleIcon}</span>
+                        </div>
+                        <div id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                            Your Password must have:
+                            <ul>
+                                <li className={pwd.length > 8 ? 'green' : 'rot'}> 8 or more symbols</li>
+                                <li className={/[0-9]/i.test(pwd) ? 'green' : 'rot'}>1 or more numbers</li>
+                                <li className={/[a-zA-Z]/i.test(pwd) ? 'green' : 'rot'}>1 or more Latin letters</li>
+                                <li className={/[~!@#$%^&*()_+`-]/i.test(pwd) ? 'green' : 'rot'}>1 or more special characters</li>
+                            </ul>
+                        </div>
+                        <label for="confirmPassword">One more time</label>
+                        <div>
+                            <input type={passwordInputType}
+                                id="confrimPassword"
+                                onChange={(e) => setMatchPwd(e.target.value)}
+                                required
+                                aria-invalid={validMatch ? "false" : "true"}
+                                aria-describedby="confirmnote"
+                                onFocus={() => setMatchFocus(true)}
+                                onBlur={() => setMatchFocus(false)}></input>
+                            <span className='password-toggle-icon'>{toggleIcon}</span>
+                        </div>
+                        <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"} >
+                            Must match the first password input field.
+                        </p>
+                        <button className='btn' disabled={!validEmail || !validPwd || !validMatch ? true : false}>submit</button>
+                    </form>
+                </div>
+            )}
         </div>
     )
 }
