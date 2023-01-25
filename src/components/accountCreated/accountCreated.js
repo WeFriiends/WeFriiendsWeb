7aa133dccfe6afc5ad348340a1ac65aa3ab805f7
Svitlana-G <svitlana.golubieva@gmail.com>
2023-01-25 16:37:20 +0100

    Added a confirmationCode check

--- src/components/accountCreated/accountCreated.js
+++ src/components/accountCreated/accountCreated.js
@@ -1,18 +1,44 @@
+import axios from "axios"
+import { useState } from "react"
 import Logo from "../logo/logo"
 import './accountCreated.css'
 
 
 const AccountCreated = () => {
+    const [success, setSuccess] = useState(false)
+    let location = document.location
+    // It is assumed that the link will look like this http://localhost:3000/account?code=1dfsdfsfd where 1dfsdfsfd is confirmationCode
+    let confirmationCode = location.search.substring(6)
+    console.log(confirmationCode)
+
+
+    axios.get(`http://localhost:3001/api/auth/confirm/${confirmationCode}`, confirmationCode)
+        .then(result => {
+            console.log(result.status)
+            if (result.status === 200) { setSuccess(true) }
+        })
+        .catch(err => console.log(err))
+
+
+    console.log("success:", success)
+
     return (
-        <div className="accountCreated">
-            <img className="imgHeader" src="/img/account-header.svg" alt="" />
-            <section>
-                <Logo />
-                <p>Glad you’re here!<br />Hope, you’ll enjoy!</p>
-                <button className='btn-create'>let’s start!</button>
-            </section>
-            <img className="imgFooter" src="/img/account-footer.svg" alt="" />
-        </div>
+        <>
+            {success ? (
+                <div className="accountCreated">
+                    <img className="imgHeader" src="/img/account-header.svg" alt="" />
+                    <section>
+                        <Logo />
+                        <p>Glad you’re here!<br />Hope, you’ll enjoy!</p>
+                        <button className='btn-create'>let’s start!</button>
+                    </section>
+                    <img className="imgFooter" src="/img/account-footer.svg" alt="" />
+                </div>
+            ) : (
+                <div>Error</div>
+            )
+            }
+        </>
     )
 }
 
