b77f9392aa3c1d39562a26811e8d2af247e49055
Svitlana-G <svitlana.golubieva@gmail.com>
2023-02-02 16:55:48 +0100

    Added h1, form and button for signInMail

--- src/components/signInMail/signInMail.js
+++ src/components/signInMail/signInMail.js
@@ -1,10 +1,24 @@
 import Logo from "../logo/logo"
+import ButtonActive from "../buttonActive/buttonActive"
+import "./signInMail.css"
 
 const SignInMail = () => {
 
     return (
         <div className="signInMail">
             <Logo />
+            <section className="header">
+                <h1> Sign In</h1>
+            </section>
+            <form>
+                <label htmlFor="email">Login</label>
+                <input type="email" id="email"></input>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password">
+                </input>
+            </form>
+            <a href="#">Forgot Password?</a>
+            <ButtonActive name="sign in" />
         </div>
     )
 }
