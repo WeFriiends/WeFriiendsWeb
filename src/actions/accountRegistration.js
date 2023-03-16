3a4ce63c0463db64e1a8ddad078ce6aff0b3c94d
Svitlana-G <svitlana.golubieva@gmail.com>
2023-03-16 18:04:02 +0100

    Changed API link for email registration

--- src/actions/accountRegistration.js
+++ src/actions/accountRegistration.js
@@ -2,17 +2,19 @@ import axios from "axios";
 
 const accountRegistration = async (password1, password2, email) => {
   try {
-    const result = await axios.post("http://localhost:3001/api/auth/register", {
-      password: password1,
-      password2: password2,
-      email: email,
-    });
+    const result = await axios.post(
+      "https://clumsy-glasses-clam.cyclic.app/api/auth/register",
+      {
+        password: password1,
+        password2: password2,
+        email: email,
+      }
+    );
 
     if (result.status === 200) return true;
     else return false;
-    
   } catch (err) {
-    console.log(err);
+    console.log(err.response);
   }
 };
 
