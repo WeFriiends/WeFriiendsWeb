20c2c3841ff7ea7b4e645ab1b10966bdb2722047
Svitlana-G <svitlana.golubieva@gmail.com>
2023-06-16 16:42:02 +0200

    Changed localhost to clumsy-glasses-clam.cyclic.app

--- src/actions/accountRegistration.ts
+++ src/actions/accountRegistration.ts
@@ -7,8 +7,8 @@ const accountRegistration = async (
 ) => {
   try {
     const result = await axios.post(
-      // "https://clumsy-glasses-clam.cyclic.app/api/auth/register",
-      'http://localhost:3002/api/auth/register',
+      'https://clumsy-glasses-clam.cyclic.app/api/auth/register',
+      // 'http://localhost:3002/api/auth/register',
       {
         password: password1,
         password2: password2,
