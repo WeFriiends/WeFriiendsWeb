598e84a8c21ab305fd20a590d2a6c01709456349
Svitlana-G <svitlana.golubieva@gmail.com>
2023-06-19 17:49:47 +0200

    MUI styling for logo

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,13 +1,27 @@
-import './logo.css'
+import { Box } from '@mui/material'
+import { makeStyles } from 'tss-react/mui'
 
 const Logo = () => {
+  const { classes } = useStyles()
   return (
-    <div className="logo">
-      <section>
-        <img src="/img/logo.svg" alt="" />
-      </section>
-    </div>
+    <Box>
+      <Box
+        component="img"
+        src="/img/logo.svg"
+        alt="logo"
+        className={classes.logo}
+      ></Box>
+    </Box>
   )
 }
 
 export default Logo
+
+const useStyles = makeStyles()(() => {
+  return {
+    logo: {
+      marginTop: '5vh',
+      textAlign: 'center',
+    },
+  }
+})
