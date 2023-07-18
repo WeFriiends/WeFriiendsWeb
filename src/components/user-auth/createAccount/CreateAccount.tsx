9ad27daeb4c1b67a5f38ed147f58bbbb3247a91f
Svitlana-G <svitlana.golubieva@gmail.com>
2023-07-18 14:38:20 +0200

    Change styles for responsive design, page create account

--- src/components/user-auth/createAccount/CreateAccount.tsx
+++ src/components/user-auth/createAccount/CreateAccount.tsx
@@ -1,16 +1,11 @@
 import Logo from '../../logo/Logo'
-import { Button, Grid, Typography, Box, Link } from '@mui/material'
+import { Typography, Box, Link } from '@mui/material'
 import { makeStyles } from 'tss-react/mui'
 
 const CreateAccount = () => {
   const { classes } = useStyles()
   return (
-    <Box
-      mr={2.5}
-      ml={2.5}
-      height="100vh"
-      sx={{ display: 'grid', gridTemplateRows: '1fr 2fr 4fr 1fr' }}
-    >
+    <Box className={classes.mainBox}>
       <Box>
         <Logo />
       </Box>
@@ -21,39 +16,25 @@ const CreateAccount = () => {
         <Typography className={classes.subTitle}>Create an account</Typography>
       </Box>
       <Box>
-        <Grid container spacing={2.5}>
-          {/* <Grid item xs={12}>
-            <Button
-              fullWidth
-              variant="contained"
-              className={classes.fbAndGoogleButton}
-              startIcon={<img alt="fb" src={'/img/fb.svg'} />}
-            >
-              Facebook
-            </Button>
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              fullWidth
-              variant="contained"
-              className={classes.fbAndGoogleButton}
-              startIcon={<img alt="google" src={'/img/google.svg'} />}
-            >
-              Google
-            </Button>
-          </Grid> */}
-          <Grid item xs={12}>
-            <Link href="/registration">
-              <Button
-                fullWidth
-                variant="contained"
-                className={classes.emailButton}
-              >
-                e-mail
-              </Button>
-            </Link>
-          </Grid>
-        </Grid>
+        {/* <Button
+          fullWidth
+          variant="contained"
+          className={classes.fbAndGoogleButton}
+          startIcon={<img alt="fb" src={'/img/fb.svg'} />}
+        >
+          Facebook
+        </Button>
+        <Button
+          fullWidth
+          variant="contained"
+          className={classes.fbAndGoogleButton}
+          startIcon={<img alt="google" src={'/img/google.svg'} />}
+        >
+          Google
+        </Button> */}
+        <Link href="/registration" className={classes.linkBtn}>
+          e-mail
+        </Link>
         <Typography className={classes.p}>
           By creating an account, I agree with
           <Link className={classes.linkSmall}> The Terms of Service </Link>
@@ -75,8 +56,19 @@ const CreateAccount = () => {
 
 export default CreateAccount
 
-const useStyles = makeStyles()(() => {
+const useStyles = makeStyles()((theme) => {
   return {
+    mainBox: {
+      display: 'grid',
+      gridTemplateRows: '1fr 2fr 2fr 1fr',
+      alignItems: 'center',
+      marginLeft: 20,
+      marginRight: 20,
+      [theme.breakpoints.up('sm')]: {
+        width: 400,
+        margin: '0 auto',
+      },
+    },
     title: {
       fontSize: 32,
       fontWeight: 600,
@@ -103,13 +95,17 @@ const useStyles = makeStyles()(() => {
       color: '#1D878C',
       fontSize: 13,
     },
-    emailButton: {
+    linkBtn: {
+      display: 'block',
       textTransform: 'lowercase',
       backgroundColor: '#FFF1EC',
       color: '#444444',
-      height: 56,
+      paddingTop: 18,
+      paddingBottom: 18,
+      borderRadius: 10,
       fontSize: 18,
       textDecoration: 'none',
+      textAlign: 'center',
     },
     fbAndGoogleButton: {
       textTransform: 'capitalize',
@@ -119,7 +115,7 @@ const useStyles = makeStyles()(() => {
       fontSize: 18,
     },
     p: {
-      paddingTop: 15,
+      paddingTop: 14,
       textAlign: 'left',
       fontSize: 13,
     },
