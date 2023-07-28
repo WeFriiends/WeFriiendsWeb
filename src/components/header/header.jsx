13894459f13c1d75e2670dd7bf055e6f129911ca
Svitlana-G <svitlana.golubieva@gmail.com>
2023-07-28 13:41:15 +0200

    Add component Header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,22 @@
+import Logo from 'components/logo/Logo'
+import Menu from 'components/menu/Menu'
+import { Box } from '@mui/material'
+import AvatarWithName from 'components/avatarWithName/AvatarWithName'
+
+const Header = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'end',
+        justifyContent: 'space-around',
+      }}
+    >
+      <Logo />
+      <Menu />
+      <AvatarWithName />
+    </Box>
+  )
+}
+
+export default Header
