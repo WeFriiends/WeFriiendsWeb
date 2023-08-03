dcf114467da10ebced40e484a1c1f395427da38d
Svitlana-G <svitlana.golubieva@gmail.com>
2023-08-03 15:27:50 +0200

    Remove unnecessary element

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,14 +2,12 @@ import { BottomNavigation, BottomNavigationAction } from '@mui/material'
 
 const Menu = () => {
   return (
-    <>
-      <BottomNavigation>
-        <BottomNavigationAction icon={<img src="/img/near_me.svg" />} />
-        <BottomNavigationAction icon={<img src="/img/like_you.svg" />} />
-        <BottomNavigationAction icon={<img src="/img/ii.svg" />} />
-        <BottomNavigationAction icon={<img src="/img/chat.svg" />} />
-      </BottomNavigation>
-    </>
+    <BottomNavigation>
+      <BottomNavigationAction icon={<img src="/img/near_me.svg" />} />
+      <BottomNavigationAction icon={<img src="/img/like_you.svg" />} />
+      <BottomNavigationAction icon={<img src="/img/ii.svg" />} />
+      <BottomNavigationAction icon={<img src="/img/chat.svg" />} />
+    </BottomNavigation>
   )
 }
 
