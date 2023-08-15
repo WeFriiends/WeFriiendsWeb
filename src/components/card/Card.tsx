405166e4b3487087575bc702ee956047c658899e
Svitlana-G <svitlana.golubieva@gmail.com>
2023-08-15 17:45:08 +0200

    Add accordion to the card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,9 +1,31 @@
-import { Box } from '@mui/material'
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Typography,
+} from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
 const Card = () => {
   return (
     <Box>
       <img src="/img/foto_Elena.jpg" alt="card" />
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>Elena S., 36</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography variant="h3">About Me</Typography>
+          <Typography>
+            I am a talented, ambitious and hardworking individual, with broad
+            skills and experience in digital and printed marketing, social media
+            and leading projects.
+          </Typography>
+          <Typography variant="h3">Education and Profession</Typography>
+          <Typography>Freelance Journalist Bachelor degree</Typography>
+        </AccordionDetails>
+      </Accordion>
     </Box>
   )
 }
