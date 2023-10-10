d8aa4b700239c80ccf07ab52b50953dc135ece68
Svitlana-G <svitlana.golubieva@gmail.com>
2023-10-10 17:25:31 +0200

    Delete margin with negative numbers and add grid for overlay

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -12,73 +12,81 @@ import { makeStyles } from 'tss-react/mui'
 const Card = () => {
   const { classes } = useStyles()
   return (
-    <Box>
-      <Box className={classes.iconsAbove}>
-        <Box className={classes.likesYou}>
-          <img src="/img/likes_me.svg" alt="likes me" />
-          <Typography>Likes you</Typography>
-        </Box>
-        <img src="/img/verified.svg"></img>
-      </Box>
-
-      <Box
-        component="img"
-        src="/img/foto_Elena.jpg"
-        alt="card"
-        className={classes.foto}
-      />
-
-      <Accordion className={classes.accordion}>
-        <AccordionSummary expandIcon={<img src="/img/arrow-down.svg" />}>
-          <Box>
-            <Box sx={{ display: 'flex' }}>
-              <Typography className={classes.name}>Elena S., 36</Typography>
-              <CircleRoundedIcon
-                className={classes.roundIcon}
-              ></CircleRoundedIcon>
-            </Box>
-            <Box className={classes.distanceWithIcon}>
-              <img src="/img/near_me.svg" height={20} width={20} />
-              <Typography className={classes.distance}>
-                from Roma, 10 km from you
-              </Typography>
-            </Box>
+    <>
+      <Box className={classes.mainGrid}>
+        <Box className={classes.iconsAbove}>
+          <Box className={classes.likesYou}>
+            <img src="/img/likes_me.svg" alt="likes me" />
+            <Typography>Likes you</Typography>
           </Box>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography variant="h3" className={classes.title}>
-            About Me
-          </Typography>
-          <Typography className={classes.text}>
-            I am a talented, ambitious and hardworking individual, with broad
-            skills and experience in digital and printed marketing, social media
-            and leading projects.
-          </Typography>
-          <Typography variant="h3" className={classes.title}>
-            Education and Profession
-          </Typography>
-          <Typography className={classes.text}>Freelance Journalist</Typography>
-          <Typography className={classes.text}>Bachelor degree</Typography>
-        </AccordionDetails>
-      </Accordion>
+          <img src="/img/verified.svg"></img>
+        </Box>
+        <Box
+          component="img"
+          src="/img/foto_Elena.jpg"
+          alt="card"
+          className={classes.foto}
+        />
+        <Accordion className={classes.accordion}>
+          <AccordionSummary expandIcon={<img src="/img/arrow-down.svg" />}>
+            <Box>
+              <Box sx={{ display: 'flex' }}>
+                <Typography className={classes.name}>Elena S., 36</Typography>
+                <CircleRoundedIcon
+                  className={classes.roundIcon}
+                ></CircleRoundedIcon>
+              </Box>
+              <Box className={classes.distanceWithIcon}>
+                <img src="/img/near_me.svg" height={20} width={20} />
+                <Typography className={classes.distance}>
+                  from Roma, 10 km from you
+                </Typography>
+              </Box>
+            </Box>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography variant="h3" className={classes.title}>
+              About Me
+            </Typography>
+            <Typography className={classes.text}>
+              I am a talented, ambitious and hardworking individual, with broad
+              skills and experience in digital and printed marketing, social
+              media and leading projects.
+            </Typography>
+            <Typography variant="h3" className={classes.title}>
+              Education and Profession
+            </Typography>
+            <Typography className={classes.text}>
+              Freelance Journalist
+            </Typography>
+            <Typography className={classes.text}>Bachelor degree</Typography>
+          </AccordionDetails>
+        </Accordion>
+      </Box>
       <Box className={classes.buttonSection}>
         <Button className={classes.whiteButton}> Skip </Button>
         <Button className={classes.orangeButton}>Be friend</Button>
       </Box>
-    </Box>
+    </>
   )
 }
 export default Card
 
 const useStyles = makeStyles()(() => {
   return {
+    mainGrid: {
+      display: ' grid',
+      gridTemplateRows: 'repeat(9, 1fr)',
+      maxHeight: '65vh',
+    },
     iconsAbove: {
       display: 'flex',
       justifyContent: 'space-between',
+      alignItems: 'center',
       zIndex: 1,
-      position: 'relative',
-      marginBottom: -66,
       padding: '0 11px',
+      gridRow: '1/1',
+      gridColumn: '1/2',
     },
     likesYou: {
       display: 'flex',
@@ -161,19 +169,14 @@ const useStyles = makeStyles()(() => {
       lineHeight: '22px',
     },
     foto: {
-      height: '100%',
       width: '100%',
+      gridRow: '1/9',
+      gridColumn: '1/2',
     },
     accordion: {
-      marginTop: -212,
       paddingTop: 106,
       background:
         'linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 49.79%)',
-      '&.Mui-expanded ': {
-        margin: '-212px 0 0 0',
-        height: 'auto',
-        paddingTop: 90,
-      },
       '&::before': {
         height: 0,
       },
