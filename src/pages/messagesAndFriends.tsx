36c92e09c0aaab4514c93950db6945e517231f9c
Svitlana-G <svitlana.golubieva@gmail.com>
2023-11-07 20:26:03 +0100

    Rename tabsMessagesFriends

--- src/pages/messagesAndFriends.tsx
+++ src/pages/messagesAndFriends.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/material'
 import Header from 'components/header/Header'
-import TabsMessagesFriends from 'components/tabsMessegeFriends/TabsMessegesFriends'
+import TabsMessagesFriends from 'components/tabsMessegeFriends/TabsMessagesFriends'
 import Card from 'components/card/Card'
 
 const MessagesAndFriends = () => {
