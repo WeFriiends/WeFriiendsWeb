dfa76361c31e1aee8936557968e67ddd47c629f6
nasnik <nikoulkina@gmail.com>
2023-11-07 14:54:32 -0800

    eslint and prettier mistakes resolved

--- src/components/nearMe/NearMeProfiles.tsx
+++ src/components/nearMe/NearMeProfiles.tsx
@@ -1,70 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import { Grid, Typography } from '@mui/material';
-import { makeStyles } from 'tss-react/mui';
-import { getUsersNearMeData } from '../../actions/userServices';
-import { getColumns } from '../../helpers/helper';
-import UserListRenderer from './UserListRenderer';
+import React, { useEffect, useState } from 'react'
+import { Grid, Typography } from '@mui/material'
+import { makeStyles } from 'tss-react/mui'
+import { getUsersNearMeData } from '../../actions/userServices'
+import { getColumns } from '../../helpers/helper'
+import UserListRenderer from './UserListRenderer'
 
-import {UserObjectType} from '../../helpers/userTypes';
+import { UserObjectType } from '../../helpers/userTypes'
 type IsMobileProps = {
-    isMobile: boolean;
-};
+  isMobile: boolean
+}
 
-    const NearMeProfiles = ({ isMobile }: IsMobileProps) => {
-
-    const {classes} = useStyles();
-    const [list, setList] = useState<Array<UserObjectType>>([]);
-    const columns = getColumns(isMobile);
-    const txtAlign = isMobile ? 'center' : 'left';
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const result = await getUsersNearMeData();
-                setList(result);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        }
-        fetchData();
-    }, []);
-
-    return (
-        <Grid container spacing={3}>
-            <Grid item xs={12}>
-                <Typography className={classes.headerNear} sx={{ textAlign: txtAlign }}>
-                    Near by
-                </Typography>
-                <Typography className={classes.description} sx={{ textAlign: txtAlign }}>
-                    These people near you – just like them and see if it’s a match!
-                </Typography>
-            </Grid>
-            <Grid item xs={12}>
-                    <UserListRenderer users={list} classes={classes} columns={columns}/>
-            </Grid>
-        </Grid>
-    );
-};
-export default NearMeProfiles;
-const useStyles = makeStyles()(() => {
-    return {
-        headerNear: {
-            color: '#F1562A',
-            fontSize: 32,
-            fontFamily: 'Inter',
-            fontWeight: '600',
-        },
-        description: {
-            width: '100%',
-            color: 'black',
-            fontSize: 14,
-            fontFamily: 'Inter',
-            fontWeight: '400',
-            wordWrap: 'break-word',
-        },
-        userImages: {
-            borderRadius: '50%',
-            width: '100px',
-            height: '100px',
-        },
+const NearMeProfiles = ({ isMobile }: IsMobileProps) => {
+  const { classes } = useStyles()
+  const [list, setList] = useState<Array<UserObjectType>>([])
+  const columns = getColumns(isMobile)
+  const txtAlign = isMobile ? 'center' : 'left'
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const result = await getUsersNearMeData()
+        setList(result)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
     }
+    fetchData()
+  }, [])
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Typography className={classes.headerNear} sx={{ textAlign: txtAlign }}>
+          Near by
+        </Typography>
+        <Typography
+          className={classes.description}
+          sx={{ textAlign: txtAlign }}
+        >
+          These people near you – just like them and see if it’s a match!
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <UserListRenderer users={list} classes={classes} columns={columns} />
+      </Grid>
+    </Grid>
+  )
+}
+export default NearMeProfiles
+const useStyles = makeStyles()(() => {
+  return {
+    headerNear: {
+      color: '#F1562A',
+      fontSize: 32,
+      fontFamily: 'Inter',
+      fontWeight: '600',
+    },
+    description: {
+      width: '100%',
+      color: 'black',
+      fontSize: 14,
+      fontFamily: 'Inter',
+      fontWeight: '400',
+      wordWrap: 'break-word',
+    },
+    userImages: {
+      borderRadius: '50%',
+      width: '100px',
+      height: '100px',
+    },
+  }
 })
--- src/components/nearMe/UserListRenderer.tsx
+++ src/components/nearMe/UserListRenderer.tsx
@@ -1,46 +1,52 @@
-import React from 'react';
-import { Box, ImageList, ImageListItem, Typography } from '@mui/material';
-import { LocationOn } from '@mui/icons-material';
-import {UserObjectType} from '../../helpers/userTypes';
-import Lightning from "../lightning/Lightning";
+import React from 'react'
+import { Box, ImageList, ImageListItem, Typography } from '@mui/material'
+import { LocationOn } from '@mui/icons-material'
+import { UserObjectType } from '../../helpers/userTypes'
+import Lightning from '../lightning/Lightning'
 
 type UserListRendererProps = {
-    users: UserObjectType[];
-    classes: any;
-    columns: number;
-};
+  users: UserObjectType[]
+  classes: any
+  columns: number
+}
 
-const UserListRenderer: React.FC<UserListRendererProps> = ({ users, classes, columns }) => {
-    const currentUserID = '1';//take it from localStorage
-    return (
-        <ImageList cols={columns}>
-            {users
-                .filter((user) => user.location === 'Istanbul')
-                .map((user) => (
-                    <ImageListItem key={user.id} sx={{ marginBottom: 2 }}>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                justifyContent: 'center',
-                            }}
-                        >
-                            <img src={user.picture} className={classes.userImages} alt="user profile" />
-                            {user.likedUsers.includes(currentUserID)&&
-                                <Lightning />
-                            }
-                            <Box sx={{ fontWeight: 'bold', color: '#F46B5D' }}>
-                                {user.fName} {user.lName}
-                            </Box>
-                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                                <LocationOn sx={{ color: 'grey' }} />
-                                <Typography sx={{ color: 'grey' }}>1 km</Typography>
-                            </Box>
-                        </Box>
-                    </ImageListItem>
-                ))}
-        </ImageList>
-    );
-};
+const UserListRenderer: React.FC<UserListRendererProps> = ({
+  users,
+  classes,
+  columns,
+}) => {
+  const currentUserID = '1' //take it from localStorage
+  return (
+    <ImageList cols={columns}>
+      {users
+        .filter((user) => user.location === 'Istanbul')
+        .map((user) => (
+          <ImageListItem key={user.id} sx={{ marginBottom: 2 }}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+              }}
+            >
+              <img
+                src={user.picture}
+                className={classes.userImages}
+                alt="user profile"
+              />
+              {user.likedUsers.includes(currentUserID) && <Lightning />}
+              <Box sx={{ fontWeight: 'bold', color: '#F46B5D' }}>
+                {user.fName} {user.lName}
+              </Box>
+              <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                <LocationOn sx={{ color: 'grey' }} />
+                <Typography sx={{ color: 'grey' }}>1 km</Typography>
+              </Box>
+            </Box>
+          </ImageListItem>
+        ))}
+    </ImageList>
+  )
+}
 
-export default UserListRenderer;
+export default UserListRenderer
