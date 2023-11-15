7215b7e8c7b6167225749f130c8fc853798cb742
Svitlana-G <svitlana.golubieva@gmail.com>
2023-11-15 10:36:34 +0100

    Output name, lastname and age

--- src/components/tabsMessegeFriends/TabsMessagesFriends.tsx
+++ src/components/tabsMessegeFriends/TabsMessagesFriends.tsx
@@ -76,8 +76,11 @@ const TabsMessagesFriends = () => {
               <img
                 src={element.foto}
                 alt="photo"
-                className={classes.smallPhot}
+                className={classes.smallPhoto}
               />
+              <Typography className={classes.textOnPhoto}>
+                {element.name} {element.lastname}, {element.age}
+              </Typography>
             </Box>
           ))}
         </Box>
@@ -134,9 +137,26 @@ const useStyles = makeStyles()(() => {
     },
     friendsPhotos: {
       justifySelf: 'center',
+      display: 'grid',
+      gridTemplateRows: '1fr 71px',
     },
-    smallPhot: {
+    smallPhoto: {
       width: 162,
+      gridRow: '1/3',
+      gridColumn: '1/2',
+      boxShadow: '0px 0px 7px 1px rgba(179, 179, 179, 0.14)',
+    },
+    textOnPhoto: {
+      color: '#F46B5D',
+      gridRow: '2/3',
+      gridColumn: '1/2',
+      fontSize: 15,
+      fontWeight: 600,
+      lineHeight: '40px',
+      paddingTop: 27,
+      paddingLeft: 5,
+      background:
+        'linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 49.79%)',
     },
   }
 })
