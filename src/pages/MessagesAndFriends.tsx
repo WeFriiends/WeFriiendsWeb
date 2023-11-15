c893ff40c394472a92b7f7f023106faad2084104
Svitlana-G <svitlana.golubieva@gmail.com>
2023-11-15 10:48:42 +0100

    Fix after merge

--- src/pages/MessagesAndFriends.tsx
+++ src/pages/MessagesAndFriends.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/material'
 import Header from 'components/header/Header'
-import TabsMessagesFriends from 'components/tabsMessegeFriends/TabsMessegesFriends'
+import TabsMessagesFriends from 'components/tabsMessegeFriends/TabsMessagesFriends'
 import UserProfile from 'components/userProfile/UserProfile'
 
 const MessagesAndFriends = () => {
