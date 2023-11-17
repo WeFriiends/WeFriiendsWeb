0569a3413c81d2e90b67b29daa71f35c159cc19a
Svitlana-G <svitlana.golubieva@gmail.com>
2023-11-17 19:10:39 +0100

    Move code for messages to separate component

--- src/components/tabsMessagesFriends/TabsMessagesFriends.tsx
+++ src/components/tabsMessagesFriends/TabsMessagesFriends.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
-import { Tab, Tabs, Box, Typography, Avatar } from '@mui/material'
-import { makeStyles } from 'tss-react/mui'
-import messages from './messages.json'
+import { Tab, Tabs, Box, Typography } from '@mui/material'
+import Messages from './Messages'
 
 interface TabPanelProps {
   children?: React.ReactNode
@@ -31,7 +30,6 @@ function TabPanel(props: TabPanelProps) {
 
 const TabsMessagesFriends = () => {
   const [value, setValue] = React.useState(0)
-  const { classes } = useStyles()
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue)
@@ -43,30 +41,7 @@ const TabsMessagesFriends = () => {
         <Tab label="Friends" />
       </Tabs>
       <TabPanel value={value} index={0}>
-        {messages.map((element, index) => (
-          <>
-            <Box className={classes.messageBlock} key={index}>
-              <Avatar
-                src={element.avatar}
-                sx={{ width: 66, height: 66 }}
-              ></Avatar>
-              <Box className={classes.message}>
-                <Typography className={classes.name}>
-                  {element.name} {element.lastname}, {element.age}
-                </Typography>
-                <Typography className={classes.textMessage}>
-                  {element.message}
-                </Typography>
-              </Box>
-              {element.messageCount === '0' ? null : (
-                <Box className={classes.messageQuantity}>
-                  {element.messageCount}
-                </Box>
-              )}
-            </Box>
-            <Box className={classes.line}></Box>
-          </>
-        ))}
+        <Messages />
       </TabPanel>
       <TabPanel value={value} index={1}>
         Item Two Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -78,45 +53,3 @@ const TabsMessagesFriends = () => {
 }
 
 export default TabsMessagesFriends
-
-const useStyles = makeStyles()(() => {
-  return {
-    messageBlock: {
-      display: 'grid',
-      gridTemplateColumns: '0.5fr 5fr 0.5fr',
-      alignItems: 'center',
-      paddingBottom: 30,
-      paddingRight: 30,
-    },
-    message: {
-      paddingLeft: 16,
-      paddingRight: 31,
-    },
-    messageQuantity: {
-      borderRadius: '50%',
-      background: '#FB8F67',
-      width: 35,
-      height: 35,
-      color: '#FFFFFF',
-      fontSize: 16,
-      lineHeight: '22px',
-      fontWeight: 600,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    name: {
-      fontSize: 20,
-      fontWeight: 600,
-      lineHeight: '40px',
-    },
-    textMessage: {
-      fontSize: 14,
-      lineHeight: '22px',
-    },
-    line: {
-      borderTop: '1px solid #EEE',
-      paddingBottom: 30,
-    },
-  }
-})
