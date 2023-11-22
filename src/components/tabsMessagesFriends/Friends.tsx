7e8c721f2f1ef68b89c42f2eedf66dbce6ebf145
Svitlana-G <svitlana.golubieva@gmail.com>
2023-11-22 18:17:47 +0100

    Move the code for tab friends into separate component

---
+++ src/components/tabsMessagesFriends/Friends.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+import { Box, Typography } from '@mui/material'
+import { makeStyles } from 'tss-react/mui'
+import friends from './friends.json'
+
+const Friends = () => {
+  const { classes } = useStyles()
+  return (
+    <Box className={classes.friendsBlock}>
+      {friends.map((element, index) => (
+        <Box key={index} className={classes.friendsPhotos}>
+          <img src={element.foto} alt="photo" className={classes.smallPhoto} />
+          <Typography className={classes.textOnPhoto}>
+            {element.name} {element.lastname}, {element.age}
+          </Typography>
+        </Box>
+      ))}
+    </Box>
+  )
+}
+export default Friends
+
+const useStyles = makeStyles()(() => {
+  return {
+    line: {
+      borderTop: '1px solid #EEE',
+      paddingBottom: 30,
+    },
+    friendsBlock: {
+      display: 'grid',
+      gridTemplateColumns: '1fr 1fr',
+      gridGap: 29,
+    },
+    friendsPhotos: {
+      justifySelf: 'center',
+      display: 'grid',
+      gridTemplateRows: '1fr 71px',
+    },
+    smallPhoto: {
+      width: 162,
+      gridRow: '1/3',
+      gridColumn: '1/2',
+      boxShadow: '0px 0px 7px 1px rgba(179, 179, 179, 0.14)',
+    },
+    textOnPhoto: {
+      color: '#F46B5D',
+      gridRow: '2/3',
+      gridColumn: '1/2',
+      fontSize: 15,
+      fontWeight: 600,
+      lineHeight: '40px',
+      paddingTop: 27,
+      paddingLeft: 5,
+      background:
+        'linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 49.79%)',
+    },
+  }
+})
--- src/components/tabsMessagesFriends/TabsMessagesFriends.tsx
+++ src/components/tabsMessagesFriends/TabsMessagesFriends.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react'
 import { Tab, Tabs, Box, Typography } from '@mui/material'
-import { makeStyles } from 'tss-react/mui'
 import Messages from './Messages'
-import friends from './friends.json'
+import Friends from './Friends'
 
 interface TabPanelProps {
   children?: React.ReactNode
@@ -32,7 +31,6 @@ function TabPanel(props: TabPanelProps) {
 
 const TabsMessagesFriends = () => {
   const [value, setValue] = React.useState(0)
-  const { classes } = useStyles()
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue)
@@ -47,60 +45,10 @@ const TabsMessagesFriends = () => {
         <Messages />
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <Box className={classes.friendsBlock}>
-          {friends.map((element, index) => (
-            <Box key={index} className={classes.friendsPhotos}>
-              <img
-                src={element.foto}
-                alt="photo"
-                className={classes.smallPhoto}
-              />
-              <Typography className={classes.textOnPhoto}>
-                {element.name} {element.lastname}, {element.age}
-              </Typography>
-            </Box>
-          ))}
-        </Box>
+        <Friends />
       </TabPanel>
     </Box>
   )
 }
 
 export default TabsMessagesFriends
-
-const useStyles = makeStyles()(() => {
-  return {
-    line: {
-      borderTop: '1px solid #EEE',
-      paddingBottom: 30,
-    },
-    friendsBlock: {
-      display: 'grid',
-      gridTemplateColumns: '1fr 1fr',
-      gridGap: 29,
-    },
-    friendsPhotos: {
-      justifySelf: 'center',
-      display: 'grid',
-      gridTemplateRows: '1fr 71px',
-    },
-    smallPhoto: {
-      width: 162,
-      gridRow: '1/3',
-      gridColumn: '1/2',
-      boxShadow: '0px 0px 7px 1px rgba(179, 179, 179, 0.14)',
-    },
-    textOnPhoto: {
-      color: '#F46B5D',
-      gridRow: '2/3',
-      gridColumn: '1/2',
-      fontSize: 15,
-      fontWeight: 600,
-      lineHeight: '40px',
-      paddingTop: 27,
-      paddingLeft: 5,
-      background:
-        'linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 49.79%)',
-    },
-  }
-})
