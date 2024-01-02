343d317b0c2df10626e8f86289ed12ed2dee8e35
Svitlana-G <svitlana.golubieva@gmail.com>
2024-01-02 19:31:56 +0100

    Change to destructuring assignment

--- src/pages/MessagesAndFriends.tsx
+++ src/pages/MessagesAndFriends.tsx
@@ -5,14 +5,14 @@ import UserProfile from 'components/userProfile/UserProfile'
 import useFriendsData from 'hooks/useFriendsData'
 
 const MessagesAndFriends = () => {
-  const data = useFriendsData()
+  const { friendsData, selectFriend } = useFriendsData()
   return (
     <Box sx={{ width: '1043px', margin: '0 auto' }}>
       <Header />
       <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
-        <TabsMessagesFriends onClick={data.selectFriend} />
+        <TabsMessagesFriends onClick={selectFriend} />
         <Box sx={{ paddingLeft: '53px', paddingTop: '36px' }}>
-          <UserProfile user={data.friendsData} />
+          <UserProfile user={friendsData} />
         </Box>
       </Box>
     </Box>
