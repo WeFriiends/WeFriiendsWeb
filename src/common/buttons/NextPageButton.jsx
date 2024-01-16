a344d9f12d0cdb54413f176e7ca83f0955b9e7ed
lenayork <lenayork@tut.by>
2024-01-16 13:40:12 +0100

    feat: add next page and previous page buttons components

---
+++ src/common/buttons/NextPageButton.jsx
@@ -0,0 +1,38 @@
+import { Button } from '@mui/material'
+import { makeStyles } from 'tss-react/mui'
+
+const NextPageButton = ({ nextPageLink }) => {
+  const { classes } = useStyles()
+
+  return (
+    <Button className={classes.nextPageButton} href={nextPageLink}>
+      Next
+    </Button>
+  )
+}
+
+export default NextPageButton
+
+const useStyles = makeStyles()((theme) => {
+  return {
+    nextPageButton: {
+      width: 350,
+      height: 60,
+      padding: '18px 24px',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
+      background: '#FB8F67',
+      color: '#FFF',
+      textTransform: 'none',
+      fontFamily: 'Inter',
+      fontSize: 18,
+      fontStyle: 'normal',
+      fontWeight: 600,
+      lineHeight: 24,
+      [theme.breakpoints.up('sm')]: {
+        width: 400,
+      },
+    },
+  }
+})
