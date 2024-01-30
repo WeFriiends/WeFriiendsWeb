18e3cd1b9f6599a83eff41611d843829f141957b
lenayork <lenayork@tut.by>
2024-01-30 22:49:12 +0100

    feat: add the ability to select up to three statuses saving them into local storage

--- src/components/firstProfile/Status.tsx
+++ src/components/firstProfile/Status.tsx
@@ -14,19 +14,39 @@ import NextPageButton from '../../common/buttons/NextPageButton'
 
 const STATUSES = [
   'Looking for new friends',
-  "I'm a new mom",
-  "Let's take the dogs for a walk",
-  "Let's be friends, I'm new in town",
   "I’m learning a new language. Let's talk!",
-  'My plans are to move abroad',
-  'How do you do it? Share your experience',
+  "Let's be friends, I'm new in town",
   'I look for emotional support',
+  'My plans are to move abroad',
+  "I'm a new mom. Need some help",
+  "Let's take the dogs for a walk",
 ]
 
 const Status = () => {
   const { classes } = useStyles()
-  const [userStatus, setUserStatus] = useState('')
-  console.log('userStatus!', userStatus)
+  const [userStatus, setUserStatus] = useState<number[]>([])
+
+  const statusHandler = (event: React.MouseEvent) => {
+    const target = event.target as HTMLElement
+    const phraseId = Number(target.id.slice(-1))
+    let currentStatusList = [...userStatus]
+    if (currentStatusList.includes(phraseId)) {
+      currentStatusList = currentStatusList.filter((elem) => elem !== phraseId)
+    } else {
+      currentStatusList.push(phraseId)
+    }
+    if (currentStatusList.length > 3) {
+      currentStatusList.shift()
+    }
+    setUserStatus(currentStatusList)
+  }
+
+  const nextHandler = () => {
+    const userStatusPhrases = userStatus.map((index) => STATUSES[index])
+    localStorage.setItem('userStatus', JSON.stringify(userStatusPhrases))
+  }
+
+  const checkStatus = (index: number) => userStatus.some((num) => num === index)
 
   return (
     <Box className={classes.mainBox}>
@@ -41,19 +61,38 @@ const Status = () => {
           What are you looking for?
         </Typography>
         <Typography className={classes.subTitle}>
-          This will be you status. You can always change it
+          This will be your status. You can always change it
         </Typography>
       </Box>
-      <Box className={classes.textareaContainer}>
+      <Box className={classes.phraseWrapper}>
+        {STATUSES.map((phrase, index) => (
+          <Box
+            key={phrase}
+            className={classes.phrase}
+            onClick={statusHandler}
+            id={`phrase${index}`}
+            sx={{
+              backgroundColor: checkStatus(index)
+                ? '#faa06d'
+                : 'rgba(229, 229, 229, 0.40)',
+            }}
+          >
+            <Typography className={classes.text} id={`text${index}`}>
+              {phrase}
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+      {/* <Box className={classes.textareaContainer}>
         <TextareaAutosize
           minRows={4}
           className={classes.textarea}
           placeholder="Is there anything you would like to add?"
           onChange={(e) => setUserStatus(e.target.value)}
         />
-      </Box>
+      </Box> */}
       <Box className={classes.nextPageContainer}>
-        <NextPageButton nextPageLink="/messages" />
+        <NextPageButton nextPageLink="/status" onClick={nextHandler} />
       </Box>
     </Box>
   )
@@ -64,10 +103,10 @@ export default Status
 const useStyles = makeStyles()((theme) => {
   return {
     mainBox: {
-      // display: 'flex',
-      // flexDirection: 'column',
-      display: 'grid',
-      gridTemplateRows: '2fr 1fr 2fr 2fr 1fr',
+      display: 'flex',
+      flexDirection: 'column',
+      // display: 'grid',
+      // gridTemplateRows: '2fr 1fr 2fr 2fr 1fr',
       alignItems: 'center',
       marginLeft: 20,
       marginRight: 20,
@@ -84,7 +123,7 @@ const useStyles = makeStyles()((theme) => {
       fontSize: 18,
       fontWeight: 600,
       lineHeight: '40px',
-      paddingTop: 80,
+      // paddingTop: 80,
       color: '#444',
       textAlign: 'center',
     },
@@ -127,14 +166,18 @@ const useStyles = makeStyles()((theme) => {
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
+      marginBottom: 14,
     },
     prevPageContainer: {
       display: 'flex',
       justifyContent: 'center',
+      marginTop: 54,
+      marginBottom: 30,
     },
     nextPageContainer: {
       display: 'flex',
       justifyContent: 'center',
+      marginTop: 50,
     },
     link: {
       color: '#1D878C',
@@ -143,5 +186,46 @@ const useStyles = makeStyles()((theme) => {
       display: 'block',
       textAlign: 'center',
     },
+    phraseWrapper: {
+      minHeight: 200,
+      display: 'flex',
+      justifyContent: 'space-between',
+      gap: '10px 20px',
+      flexWrap: 'wrap',
+    },
+    phrase: {
+      minHeight: 40,
+      padding: '4px 20px',
+      color: '#444',
+      fontFamily: 'Inter',
+      fontSize: 12,
+      fontWeight: 400,
+      borderRadius: 20,
+      // background: 'rgba(229, 229, 229, 0.40)',
+      display: 'flex',
+      alignItems: 'center',
+      maxWidth: 190,
+      transition: '0.5s',
+      '&:hover': {
+        cursor: 'pointer',
+      },
+      // '& .chosenStatus': {
+      //   backgroundColor: '#faa06d',
+      // },
+    },
+    text: {
+      fontSize: 12,
+    },
+    unchosenStatus: {
+      background: 'rgba(229, 229, 229, 0.40)',
+      color: 'red',
+    },
+    chosenStatus: {
+      background: '#faa06d',
+      color: 'blue',
+    },
+    // chosenStatus: {
+    //   backgroundColor: '#faa06d',
+    // },
   }
 })
