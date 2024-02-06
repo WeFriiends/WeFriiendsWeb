e25cf5b64376c32071dc1c9a5da01a7a41d1ce40
Svitlana-G <svitlana.golubieva@gmail.com>
2024-02-06 16:11:45 +0100

    Add styles

--- src/components/userAuth/registrationForm/EmailAlreadyUsed.tsx
+++ src/components/userAuth/registrationForm/EmailAlreadyUsed.tsx
@@ -1,4 +1,37 @@
+import Logo from 'components/logo/Logo'
+import { Box, Button, Typography } from '@mui/material'
+import { makeStyles } from 'tss-react/mui'
+import { commonStyles } from 'styles/commonStyles'
+
 const EmailAlreadyUsed = () => {
-  return <div>Email already used</div>
+  const { classes } = useStyles()
+  const commonClasses = commonStyles().classes
+  return (
+    <Box className={classes.position}>
+      <Logo />
+      <Typography className={classes.title}>
+        Emai is already registered. <br /> Please
+      </Typography>
+      <Button variant="contained">sing in</Button>
+    </Box>
+  )
 }
 export default EmailAlreadyUsed
+
+const useStyles = makeStyles()(() => {
+  return {
+    position: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    title: {
+      fontWeight: 600,
+      fontSize: 22,
+      lineHeight: '40px',
+      color: '#F46B5D',
+      textAlign: 'center',
+      paddingTop: 144,
+    },
+  }
+})
