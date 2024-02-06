8ac84ad84289a5df8b335783482c7de3389ae433
Svitlana-G <svitlana.golubieva@gmail.com>
2024-02-06 16:36:02 +0100

    Correct styles and add function navigateToSignIn

--- src/components/userAuth/registrationForm/EmailAlreadyUsed.tsx
+++ src/components/userAuth/registrationForm/EmailAlreadyUsed.tsx
@@ -2,17 +2,30 @@ import Logo from 'components/logo/Logo'
 import { Box, Button, Typography } from '@mui/material'
 import { makeStyles } from 'tss-react/mui'
 import { commonStyles } from 'styles/commonStyles'
+import { useNavigate } from 'react-router-dom'
 
 const EmailAlreadyUsed = () => {
   const { classes } = useStyles()
   const commonClasses = commonStyles().classes
+  const navigate = useNavigate()
+
+  const navigateToSignIn = () => {
+    navigate('/authentication/email-sign-in')
+  }
+
   return (
-    <Box className={classes.position}>
+    <Box className={commonClasses.mainBox}>
       <Logo />
-      <Typography className={classes.title}>
-        Emai is already registered. <br /> Please
+      <Typography className={classes.headerText}>
+        E-mai is already registered. <br /> Please
       </Typography>
-      <Button variant="contained">sing in</Button>
+      <Button
+        variant="contained"
+        className={`${commonClasses.submitButton} ${classes.signInBtn}`}
+        onClick={navigateToSignIn}
+      >
+        sing in
+      </Button>
     </Box>
   )
 }
@@ -20,18 +33,17 @@ export default EmailAlreadyUsed
 
 const useStyles = makeStyles()(() => {
   return {
-    position: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    title: {
+    headerText: {
       fontWeight: 600,
       fontSize: 22,
       lineHeight: '40px',
       color: '#F46B5D',
       textAlign: 'center',
       paddingTop: 144,
+    },
+    signInBtn: {
+      width: '70%',
+      marginTop: 30,
     },
   }
 })
