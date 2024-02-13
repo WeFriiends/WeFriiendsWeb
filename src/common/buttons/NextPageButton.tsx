d0ef083876c0f1ed22553073037e07671b6df087
lenayork <lenayork@tut.by>
2024-02-13 16:11:07 +0100

    feat: add typing

--- src/common/buttons/NextPageButton.tsx
+++ src/common/buttons/NextPageButton.tsx
@@ -1,8 +1,12 @@
 import { Button } from '@mui/material'
 import { makeStyles } from 'tss-react/mui'
 import { commonStyles } from 'styles/commonStyles'
+type NextPageButtonProps = {
+  nextPageLink: string
+  onClick: () => void
+}
 
-const NextPageButton = ({ nextPageLink, onClick }) => {
+const NextPageButton = ({ nextPageLink, onClick }: NextPageButtonProps) => {
   const { classes } = useStyles()
   const commonClasses = commonStyles().classes
 
@@ -22,7 +26,6 @@ export default NextPageButton
 const useStyles = makeStyles()((theme) => {
   return {
     nextPageButton: {
-      width: 350,
       height: 60,
       padding: '18px 24px',
       textTransform: 'none',
