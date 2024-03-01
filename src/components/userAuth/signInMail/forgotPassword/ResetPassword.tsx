790eb6cfb89d639afa97658fc61fc4684e7fd932
Svitlana-G <svitlana.golubieva@gmail.com>
2024-03-01 16:17:45 +0100

    Add react-hook-form and condition for disabled button

--- src/components/userAuth/signInMail/forgotPassword/ResetPassword.tsx
+++ src/components/userAuth/signInMail/forgotPassword/ResetPassword.tsx
@@ -1,32 +1,48 @@
 import { Box, Button, FormLabel, TextField, Typography } from '@mui/material'
 import Logo from 'components/logo/Logo'
+import { useForm } from 'react-hook-form'
 import { commonStyles } from 'styles/commonStyles'
 import { makeStyles } from 'tss-react/mui'
 
 const ResetPassword = () => {
   const commonClasses = commonStyles().classes
   const { classes } = useStyles()
+  const { register, handleSubmit, watch } = useForm()
+  const inputPassword = watch('password')
+  const inputConfirmPassword = watch('confirmPassword')
+
+  const onSubmit = (data: any) => {
+    alert(JSON.stringify(data))
+  }
   return (
     <Box className={commonClasses.mainBox}>
       <Logo />
       <Typography variant="h1" className={commonClasses.title}>
         Reset your password
       </Typography>
-      <form className={classes.formStyle}>
+      <form className={classes.formStyle} onSubmit={handleSubmit(onSubmit)}>
         <FormLabel className={classes.labelStyle} sx={{ marginTop: 2.5 }}>
           New Password
         </FormLabel>
-        <TextField className={commonClasses.inputField} />
+        <TextField
+          className={commonClasses.inputField}
+          type="password"
+          {...register('password')}
+        />
         <FormLabel className={classes.labelStyle} sx={{ marginTop: 4 }}>
           One more time
         </FormLabel>
-        <TextField className={commonClasses.inputField} />
+        <TextField
+          type="password"
+          className={commonClasses.inputField}
+          {...register('confirmPassword')}
+        />
         <Button
           fullWidth
           variant="contained"
           disableElevation
           disableRipple
-          disabled
+          disabled={!inputPassword || !inputConfirmPassword}
           className={commonClasses.submitButton}
           type="submit"
         >
