9a4b8b8caedda1a13bd4b349f8b404b1ccd5d7a8
Svitlana-G <svitlana.golubieva@gmail.com>
2024-03-18 14:25:17 +0100

    Moved colour and icon definitions to separate functions

--- src/components/userAuth/signInMail/forgotPassword/ResetPassword.tsx
+++ src/components/userAuth/signInMail/forgotPassword/ResetPassword.tsx
@@ -38,6 +38,24 @@ const ResetPassword = () => {
   const onSubmit = (data: any) => {
     alert(JSON.stringify(data))
   }
+
+  const passwordHintColor = (pattern: RegExp, password: string) => {
+    if (password) {
+      if (pattern.test(password)) {
+        return '#1D878C'
+      }
+      return '#F46B5D'
+    }
+    return '#000000'
+  }
+
+  const passwordHintIcon = (pattern: RegExp, password: string) => {
+    if (password && pattern.test(password)) {
+      return '/img/check.svg'
+    }
+    return '/img/hyphen.svg'
+  }
+
   return (
     <Box className={commonClasses.mainBox}>
       <Logo />
@@ -65,53 +83,65 @@ const ResetPassword = () => {
             setPasswordFocus(true)
           }}
         />
-        {passwordFocus && !passwordRegex.test(password) && (
-          <FormHelperText component="div" className={classes.errorBox}>
-            Your Password must have:
-            <List>
-              {/.{8,}/.test(watch('password')) ? (
-                <ListItem sx={{ color: '#1D878C', padding: 0 }}>
-                  <Box component="img" src="/img/check.svg"></Box>&nbsp;8 or
-                  more symbols
-                </ListItem>
-              ) : (
-                <ListItem sx={{ color: '#F46B5D', padding: 0 }}>
-                  - &nbsp;8 or more symbols
-                </ListItem>
-              )}
-              {/[0-9]/.test(watch('password')) ? (
-                <ListItem sx={{ color: '#1D878C', padding: 0 }}>
-                  <Box component="img" src="/img/check.svg"></Box>&nbsp;1 or
-                  more numbers
-                </ListItem>
-              ) : (
-                <ListItem sx={{ color: '#F46B5D', padding: 0 }}>
-                  - &nbsp;1 or more numbers
-                </ListItem>
-              )}
-              {/[a-zA-Z]/.test(watch('password')) ? (
-                <ListItem sx={{ color: '#1D878C', padding: 0 }}>
-                  <Box component="img" src="/img/check.svg"></Box>&nbsp;1 or
-                  more Latin letterss
-                </ListItem>
-              ) : (
-                <ListItem sx={{ color: '#F46B5D', padding: 0 }}>
-                  - &nbsp;1 or more Latin letters
-                </ListItem>
-              )}
-              {/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(watch('password')) ? (
-                <ListItem sx={{ color: '#1D878C', padding: 0 }}>
-                  <Box component="img" src="/img/check.svg"></Box>&nbsp;1 or
-                  more special characters
-                </ListItem>
-              ) : (
-                <ListItem sx={{ color: '#F46B5D', padding: 0 }}>
-                  - &nbsp;1 or more special characters
-                </ListItem>
-              )}
-            </List>
-          </FormHelperText>
-        )}
+        <FormHelperText component="div" className={classes.errorBox}>
+          Your Password must have:
+          <List>
+            <ListItem
+              sx={{
+                color: passwordHintColor(/.{8,}/, password),
+                padding: 0,
+              }}
+            >
+              <Box
+                component="img"
+                src={passwordHintIcon(/.{8,}/, password)}
+              ></Box>
+              &nbsp;8 or more symbols
+            </ListItem>
+            <ListItem
+              sx={{
+                color: passwordHintColor(/[0-9]/, password),
+                padding: 0,
+              }}
+            >
+              <Box
+                component="img"
+                src={passwordHintIcon(/[0-9]/, password)}
+              ></Box>
+              &nbsp;1 or more numbers
+            </ListItem>
+            <ListItem
+              sx={{
+                color: passwordHintColor(/[a-zA-Z]/, password),
+                padding: 0,
+              }}
+            >
+              <Box
+                component="img"
+                src={passwordHintIcon(/[a-zA-Z]/, password)}
+              ></Box>
+              &nbsp;1 or more Latin letters
+            </ListItem>
+            <ListItem
+              sx={{
+                color: passwordHintColor(
+                  /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/,
+                  password
+                ),
+                padding: 0,
+              }}
+            >
+              <Box
+                component="img"
+                src={passwordHintIcon(
+                  /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/,
+                  password
+                )}
+              ></Box>
+              &nbsp;1 or more special characters
+            </ListItem>
+          </List>
+        </FormHelperText>
         <FormLabel className={classes.labelStyle} sx={{ marginTop: 4 }}>
           One more time
         </FormLabel>
@@ -171,5 +201,6 @@ const useStyles = makeStyles()({
     borderRadius: 10,
     padding: '17px 0 20px 23px',
     marginTop: 20,
+    fontWeight: 500,
   },
 })
