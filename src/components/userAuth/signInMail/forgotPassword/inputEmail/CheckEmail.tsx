7466a7febcceb47ba8e727dd8ff412d610c4ccd4
Svitlana-G <svitlana.golubieva@gmail.com>
2024-03-21 17:36:39 +0100

    Add component check email

---
+++ src/components/userAuth/signInMail/forgotPassword/inputEmail/CheckEmail.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material'
+
+const CheckEmail = () => {
+  return (
+    <Box>
+      <Typography variant="h1">Please, check your e-mail to proceed</Typography>
+      <img src="/img/check_email.jpg" alt="check your email" />
+    </Box>
+  )
+}
+export default CheckEmail
