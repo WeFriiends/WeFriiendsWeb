71f76ba35d1b5dcaf414c394ff06418a9b2fb412
Marina Kotelnikov <oce4ka@gmail.com>
2024-04-04 21:03:44 +0200

    fix: icon location props have been removed

--- src/common/IconLocation.tsx
+++ src/common/IconLocation.tsx
@@ -2,12 +2,8 @@ import React from 'react'
 import { makeStyles } from 'tss-react/mui'
 import theme from '../styles/createTheme'
 
-type IconLocationProps = {
-  color?: string
-}
-const IconLocation: React.FC<IconLocationProps> = ({ color }) => {
+const IconLocation = () => {
   const { classes } = useStyles()
-  color ??= theme.palette.text.primary
 
   return (
     <svg
@@ -20,13 +16,13 @@ const IconLocation: React.FC<IconLocationProps> = ({ color }) => {
     >
       <path
         d="M8.93806 4.88895C8.93806 7.86812 5.1077 10.4217 5.1077 10.4217C5.1077 10.4217 1.27734 7.86812 1.27734 4.88895C1.27734 3.87308 1.6809 2.89881 2.39923 2.18048C3.11756 1.46215 4.09183 1.05859 5.1077 1.05859C6.12357 1.05859 7.09784 1.46215 7.81617 2.18048C8.5345 2.89881 8.93806 3.87308 8.93806 4.88895Z"
-        stroke={color}
+        stroke={theme.palette.text.primary}
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M5.10686 6.16295C5.81201 6.16295 6.38365 5.59131 6.38365 4.88616C6.38365 4.18101 5.81201 3.60938 5.10686 3.60938C4.40171 3.60938 3.83008 4.18101 3.83008 4.88616C3.83008 5.59131 4.40171 6.16295 5.10686 6.16295Z"
-        stroke={color}
+        stroke={theme.palette.text.primary}
         strokeLinecap="round"
         strokeLinejoin="round"
       />
