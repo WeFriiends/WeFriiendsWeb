f7db31cc705e826e331ae691e23c2b4573b9c141
Lyubov Sugak <L.Sugak84@gmail.com>
2024-04-09 15:07:31 +0200

    feat: type theme file

--- src/styles/createTheme.tsx
+++ src/styles/createTheme.tsx
@@ -6,8 +6,17 @@ const red = '#F1562A'
 const grey = '#444444'
 const green = '#1D878C'
 
-const theme: Record<string, any> = createTheme({
+const theme = {
   spacing: 5,
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1024,
+      xl: 1536,
+    },
+  },
   palette: {
     primary: {
       main: rose,
@@ -47,6 +56,16 @@ const theme: Record<string, any> = createTheme({
       fontWeight: 400,
     },
   },
-})
+} as const
 
-export default theme
+type CustomTheme = {
+  [Key in keyof typeof theme]: (typeof theme)[Key]
+}
+
+declare module '@mui/material/styles' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface Theme extends CustomTheme {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface ThemeOptions extends CustomTheme {}
+}
+export default createTheme(theme)
