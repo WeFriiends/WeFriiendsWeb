09edb606939b32cc7c5270acc2a49b67a913f494
Lyubov Sugak <L.Sugak84@gmail.com>
2024-04-15 21:52:57 +0200

    fix: auth start page

--- src/components/userAuth/createAccount/CreateAccount.tsx
+++ src/components/userAuth/createAccount/CreateAccount.tsx
@@ -1,4 +1,4 @@
-import { Typography, Box, Link } from '@mui/material'
+import { Typography, Box, Link, Button } from '@mui/material'
 import { makeStyles } from 'tss-react/mui'
 import { useAuth0 } from '@auth0/auth0-react'
 
@@ -6,8 +6,14 @@ import Logo from '../../logo/Logo'
 import { commonStyles } from 'styles/commonStyles'
 import Loader from 'common/Loader'
 
-const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0()
+const CreateAccount = () => {
+  const { classes } = useStyles()
+  const commonClasses = commonStyles().classes
+  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0()
+
+  if (isLoading) {
+    return <Loader />
+  }
 
   const handleLogin = async () => {
     await loginWithRedirect({
@@ -16,11 +22,6 @@ const LoginButton = () => {
       },
     })
   }
-
-  return <button onClick={handleLogin}>Sign In</button>
-}
-const SignupButton = () => {
-  const { loginWithRedirect } = useAuth0()
 
   const handleSignUp = async () => {
     await loginWithRedirect({
@@ -34,39 +35,6 @@ const SignupButton = () => {
   }
 
   return (
-    <button className="button__sign-up" onClick={handleSignUp}>
-      Sign Up
-    </button>
-  )
-}
-
-export const LogoutButton = () => {
-  const { logout } = useAuth0()
-
-  const handleLogout = () => {
-    logout({
-      logoutParams: {
-        returnTo: window.location.origin,
-      },
-    })
-  }
-
-  return (
-    <button className="button__logout" onClick={handleLogout}>
-      Log Out
-    </button>
-  )
-}
-
-const CreateAccount = () => {
-  const { classes } = useStyles()
-  const commonClasses = commonStyles().classes
-  const { isAuthenticated, isLoading } = useAuth0()
-
-  if (isLoading) {
-    return <Loader />
-  }
-  return (
     <Box className={`${commonClasses.mainBox} ${classes.mainGrid}`}>
       <Box>
         <Logo />
@@ -78,24 +46,14 @@ const CreateAccount = () => {
         <Typography className={commonClasses.subTitle}>
           Create an account
         </Typography>
-        {!isAuthenticated && (
-          <>
-            <SignupButton />
-            <LoginButton />
-          </>
-        )}
-        {isAuthenticated && (
-          <>
-            <LogoutButton />
-          </>
-        )}
       </Box>
       <Box>
-        {/* <Button
+        <Button
           fullWidth
           variant="contained"
           className={classes.fbAndGoogleButton}
           startIcon={<img alt="fb" src={'/img/fb.svg'} />}
+          onClick={handleSignUp}
         >
           Facebook
         </Button>
@@ -104,15 +62,18 @@ const CreateAccount = () => {
           variant="contained"
           className={classes.fbAndGoogleButton}
           startIcon={<img alt="google" src={'/img/google.svg'} />}
+          onClick={handleSignUp}
         >
           Google
-        </Button> */}
-        <Link
-          href="/registration/register-email"
+        </Button>
+        <Button
+          fullWidth
+          variant="contained"
           className={commonClasses.linkBtn}
+          onClick={handleSignUp}
         >
           e-mail
-        </Link>
+        </Button>
         <Typography className={commonClasses.p}>
           By creating an account, I agree with
           <Link
@@ -138,10 +99,14 @@ const CreateAccount = () => {
         <Typography className={commonClasses.text}>
           Already have an account?
         </Typography>
-
-        {/* <Link href="/authentication/sign-in" className={commonClasses.link}>
+        <Button
+          fullWidth
+          variant="contained"
+          className={commonClasses.linkSmall}
+          onClick={handleLogin}
+        >
           Sign In
-        </Link> */}
+        </Button>
       </Box>
     </Box>
   )
