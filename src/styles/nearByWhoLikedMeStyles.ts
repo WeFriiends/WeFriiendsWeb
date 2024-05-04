64c9420adaba1f4d27fb55c0a347ce6a5f905001
Marina Kotelnikov <oce4ka@gmail.com>
2024-05-04 17:29:34 +0200

    fix: "return" has been removed and "theme" has been moved accordingly code reviewer's demand

--- src/styles/nearByWhoLikedMeStyles.ts
+++ src/styles/nearByWhoLikedMeStyles.ts
@@ -1,102 +1,101 @@
 import { makeStyles } from 'tss-react/mui'
+import theme from '../styles/createTheme'
 
-export const nearByWhoLikedMeStyles = makeStyles()((theme) => {
-  return {
-    userImages: {
-      borderRadius: '50%',
-      width: 81,
-      height: 81,
-      [theme.breakpoints.up('lg')]: {
-        width: 135,
-        height: 135,
-      },
+export const nearByWhoLikedMeStyles = makeStyles()(() => ({
+  userImages: {
+    borderRadius: '50%',
+    width: 81,
+    height: 81,
+    [theme.breakpoints.up('lg')]: {
+      width: 135,
+      height: 135,
     },
-    imageList: {
-      gridTemplateColumns: 'repeat(auto-fill, minmax(120px, 1fr))!important',
-      maxWidth: 700,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      [theme.breakpoints.up('lg')]: {
-        gridTemplateColumns: 'repeat(5, 1fr) !important',
-        maxWidth: '100vw',
-      },
+  },
+  imageList: {
+    gridTemplateColumns: 'repeat(auto-fill, minmax(120px, 1fr))!important',
+    maxWidth: 700,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    [theme.breakpoints.up('lg')]: {
+      gridTemplateColumns: 'repeat(5, 1fr) !important',
+      maxWidth: '100vw',
     },
-    imageListItem: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      margin: '0 auto 30px',
-      cursor: 'pointer',
-      width: '100%',
+  },
+  imageListItem: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: '0 auto 30px',
+    cursor: 'pointer',
+    width: '100%',
+    '& path': {
+      // lightning icon color mobile
+      stroke: theme.palette.primary.main,
+    },
+    [theme.breakpoints.up('lg')]: {
+      margin: '0 auto 45px',
       '& path': {
-        // lightning icon color mobile
-        stroke: theme.palette.primary.main,
-      },
-      [theme.breakpoints.up('lg')]: {
-        margin: '0 auto 45px',
-        '& path': {
-          // lightning icon color desktop
-          stroke: '#AFB1B6',
-          transition: 'stroke 0.3s',
-        },
-      },
-      '&:hover path': {
-        // lightning icon hover color desktop
-        stroke: theme.palette.primary.main,
-      },
-      '& aside path': {
-        stroke: theme.palette.text.primary,
-      },
-      '&:hover aside path': {
-        stroke: theme.palette.text.primary,
-      },
-      '&:hover h4': {
-        color: theme.palette.primary.main,
+        // lightning icon color desktop
+        stroke: '#AFB1B6',
+        transition: 'stroke 0.3s',
       },
     },
-    profileBoxPosition: {
-      textAlign: 'center',
+    '&:hover path': {
+      // lightning icon hover color desktop
+      stroke: theme.palette.primary.main,
     },
-    lightingIconPosition: {
-      position: 'absolute',
-      top: 10,
-      right: 'calc(50% - 40px)',
-      width: 18,
-      height: 20,
-      [theme.breakpoints.up('lg')]: {
-        top: 3,
-        right: 30,
-        width: 30,
-        height: 33,
-      },
+    '& aside path': {
+      stroke: theme.palette.text.primary,
     },
-    usernameStyling: {
-      fontWeight: 500,
-      color: theme.palette.common.black,
-      fontSize: 12,
-      lineHeight: '20px',
-      padding: '5px 5px 0',
-      transition: 'color 0.3s',
-      '&:hover': {
-        color: theme.palette.primary.main,
-      },
-      [theme.breakpoints.up('lg')]: {
-        fontSize: 16,
-        paddingTop: 15,
-      },
+    '&:hover aside path': {
+      stroke: theme.palette.text.primary,
     },
-    distanceBoxPosition: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      paddingTop: 3,
+    '&:hover h4': {
+      color: theme.palette.primary.main,
     },
-    locationTextStyle: {
-      color: theme.palette.text.primary,
-      fontSize: 12,
-      lineHeight: '20px',
-      fontWeight: '500',
-      paddingLeft: 4,
+  },
+  profileBoxPosition: {
+    textAlign: 'center',
+  },
+  lightingIconPosition: {
+    position: 'absolute',
+    top: 10,
+    right: 'calc(50% - 40px)',
+    width: 18,
+    height: 20,
+    [theme.breakpoints.up('lg')]: {
+      top: 3,
+      right: 30,
+      width: 30,
+      height: 33,
     },
-  }
-})
+  },
+  usernameStyling: {
+    fontWeight: 500,
+    color: theme.palette.common.black,
+    fontSize: 12,
+    lineHeight: '20px',
+    padding: '5px 5px 0',
+    transition: 'color 0.3s',
+    '&:hover': {
+      color: theme.palette.primary.main,
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontSize: 16,
+      paddingTop: 15,
+    },
+  },
+  distanceBoxPosition: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 3,
+  },
+  locationTextStyle: {
+    color: theme.palette.text.primary,
+    fontSize: 12,
+    lineHeight: '20px',
+    fontWeight: '500',
+    paddingLeft: 4,
+  },
+}))
