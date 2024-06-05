8c0444b7defdc6e8545f326ce415a9fd4d0c8235
Lyubov Sugak <L.Sugak84@gmail.com>
2024-06-05 21:15:45 +0200

    fix: TS

--- src/components/userAuth/AuthGuard.tsx
+++ src/components/userAuth/AuthGuard.tsx
@@ -1,8 +1,8 @@
-import type { FC, ReactElement, ReactNode } from 'react'
+import type { FC, ComponentType } from 'react'
 import { withAuthenticationRequired } from '@auth0/auth0-react'
 import Loader from 'common/Loader'
 
-const AuthGuard: FC<any> = ({ component }) => {
+const AuthGuard: FC<{ component: ComponentType<object> }> = ({ component }) => {
   const Component = withAuthenticationRequired(component, {
     //instance of a React component that will be rendered while the user is being redirected to the login page
     //instead of loader you can use animation with moving I sticks
