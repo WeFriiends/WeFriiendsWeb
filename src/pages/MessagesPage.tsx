5940768ed92caa5aa13733e77133b5cba68b5932
Svitlana-G <svitlana.golubieva@gmail.com>
2024-06-14 16:50:25 +0200

    Add text and icons to the chat if the conversation hasn't started yet

--- src/pages/MessagesPage.tsx
+++ src/pages/MessagesPage.tsx
@@ -1,19 +1,50 @@
 import { Box, Typography } from '@mui/material'
+import { makeStyles } from 'tss-react/mui'
 import Messages from 'components/tabsMessagesFriends/Messages'
+import theme from './../styles/createTheme'
 
 const MessagesPage = () => {
+  const { classes } = useStyles()
   return (
     <Box>
       <Box sx={{ display: 'grid', gridTemplateColumns: '5.5fr 6.5fr' }}>
         <Messages />
-
         <Box>
-          <Typography>
-            Connect with others by sharing your thoughts or experiences.
-          </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
+            <Typography className={classes.startChattingText}>
+              Connect with others by sharing your thoughts or experiences.
+            </Typography>
+            <Typography className={classes.startChattingText}>
+              Start chatting now!
+            </Typography>
+          </Box>
+          <Box className={classes.iconsSection}>
+            <img src="/img/messages/friendly.svg" alt="friendly" />
+            <img src="/img/messages/hospitable.svg" alt="hospitable" />
+            <img src="/img/messages/happy.svg" alt="happy" />
+            <img src="/img/messages/love.svg" alt="love" />
+          </Box>
         </Box>
       </Box>
     </Box>
   )
 }
 export default MessagesPage
+
+const useStyles = makeStyles()({
+  startChattingText: {
+    maxWidth: '315px',
+    margin: '0 auto',
+    color: theme.palette.primary.light,
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 600,
+    lineHeight: '22px',
+  },
+  iconsSection: {
+    display: 'flex',
+    gap: 16,
+    justifyContent: 'center',
+    paddingTop: 35,
+  },
+})
