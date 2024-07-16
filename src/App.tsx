d604bae9915b84ee5d4c7c9256167d34774c426b
Marina Kotelnikov <oce4ka@gmail.com>
2024-07-16 12:40:11 +0200

    temporary fix:     </Auth0ProviderWithNavigate> has been removed to fix the project's functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { ActivePageProvider } from './context/activePageContext'
 import theme from 'styles/createTheme'
 import { DialogProvider } from 'context/dialogContext'
 import routes from 'routes'
-import { Auth0ProviderWithNavigate } from 'provider/Auth0ProviderWithNavigate'
 
 const App = () => {
   const content = useRoutes(routes)
