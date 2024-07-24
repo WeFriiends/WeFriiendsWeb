59b7cd6853dc9e1f3dbb51e9645b6814863c635d
Svitlana-G <svitlana.golubieva@gmail.com>
2024-07-24 15:14:32 +0200

    Add styles for chat menu

--- src/components/chat/ChatMenu.tsx
+++ src/components/chat/ChatMenu.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { makeStyles } from 'tss-react/mui'
 import { Button, Menu, MenuItem } from '@mui/material'
 
 const ChatMenu = () => {
+  const { classes } = useStyles()
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -25,15 +27,17 @@ const ChatMenu = () => {
       </Button>
       <Menu
         id="basic-menu"
+        className={classes.chatMenu}
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
+        style={{ top: '10px', left: '15px' }}
         MenuListProps={{
           'aria-labelledby': 'basic-button',
         }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
       >
         <MenuItem onClick={handleClose}>delete contact</MenuItem>
-        <MenuItem onClick={handleClose}>block user</MenuItem>
         <MenuItem onClick={handleClose}>report and block</MenuItem>
       </Menu>
     </div>
@@ -41,3 +45,17 @@ const ChatMenu = () => {
 }
 
 export default ChatMenu
+
+const useStyles = makeStyles()({
+  chatMenu: {
+    ' & .MuiList-root': {
+      paddingTop: 21,
+      paddingBottom: 21,
+      paddingLeft: 5,
+      paddingRight: 73,
+    },
+    'MuiPaper-root': {
+      borderRadius: 10,
+    },
+  },
+})
