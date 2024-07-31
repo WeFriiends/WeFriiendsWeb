39c1d2bdd3126da1110ba43b4d081fe03ea789a1
lenayork <lenayork@tut.by>
2024-07-31 17:32:05 +0200

    refactor: optimize code for improved performance

--- src/components/firstProfile/Gender.tsx
+++ src/components/firstProfile/Gender.tsx
@@ -1,69 +1,58 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Box, IconButton, Typography } from '@mui/material'
 import { makeStyles } from 'tss-react/mui'
 import { commonStyles } from 'styles/commonStyles'
 
 const Gender = () => {
   const { classes } = useStyles()
-  const commonClasses = commonStyles().classes
-  const [chosenGender, setChosenGender] = useState<string>('')
+  const { mainBox, title, subTitle } = commonStyles().classes
+  const [chosenGender, setChosenGender] = useState<string>('female')
   const [hoveredGender, setHoveredGender] = useState<string | null>(null)
 
-  const getMaleImage = () =>
-    chosenGender === 'male' || hoveredGender === 'male'
-      ? '/img/firstProfile/male_active.svg'
-      : '/img/firstProfile/male.svg'
-
-  const getFemaleImage = () =>
-    chosenGender === 'female' || hoveredGender === 'female'
-      ? '/img/firstProfile/female_active.svg'
-      : '/img/firstProfile/female.svg'
+  const getImage = (gender: string) => {
+    if (
+      chosenGender === gender ||
+      (hoveredGender === gender && chosenGender !== gender)
+    ) {
+      return `/img/firstProfile/${gender}_active.svg`
+    }
+    return `/img/firstProfile/${gender}.svg`
+  }
+  const genderPick = (gender: string) => {
+    setChosenGender(gender)
+  }
 
   return (
-    <Box className={commonClasses.mainBox}>
+    <Box className={mainBox}>
       <Box className={classes.titleContainer}>
-        <Typography
-          variant="h1"
-          className={`${commonClasses.title}
-          ${classes.title}`}
-        >
+        <Typography variant="h1" className={`${title} ${classes.title}`}>
           More about you
         </Typography>
       </Box>
       <Box className={classes.tipContainer}>
-        <Typography className={`${commonClasses.subTitle} ${classes.subTitle}`}>
+        <Typography className={`${subTitle} ${classes.subTitle}`}>
           What is your gender?
         </Typography>
       </Box>
       <Box className={classes.containerPosition}>
-        <Box className={classes.innerContainerPosition}>
-          <IconButton
-            onClick={() => setChosenGender('female')}
-            onMouseEnter={() => setHoveredGender('female')}
-            onMouseLeave={() => {
-              if (chosenGender !== 'female') {
-                setHoveredGender(null)
-              }
-            }}
-          >
-            <Box component="img" src={getFemaleImage()} alt="female logo" />
-          </IconButton>
-          <Typography className={classes.genderText}>Female</Typography>
-        </Box>
-        <Box className={classes.iconsPosition}>
-          <IconButton
-            onClick={() => setChosenGender('male')}
-            onMouseEnter={() => setHoveredGender('male')}
-            onMouseLeave={() => {
-              if (chosenGender !== 'male') {
-                setHoveredGender(null)
-              }
-            }}
-          >
-            <Box component="img" src={getMaleImage()} alt="male logo" />
-          </IconButton>
-          <Typography className={classes.genderText}>Male</Typography>
-        </Box>
+        {['female', 'male'].map((gender) => (
+          <Box key={gender} className={classes.innerContainerPosition}>
+            <IconButton
+              onClick={() => genderPick(gender)}
+              onMouseEnter={() => setHoveredGender(gender)}
+              onMouseLeave={() => setHoveredGender(null)}
+            >
+              <Box
+                component="img"
+                src={getImage(gender)}
+                alt={`${gender} logo`}
+              />
+            </IconButton>
+            <Typography className={classes.genderText}>
+              {gender.charAt(0).toUpperCase() + gender.slice(1)}
+            </Typography>
+          </Box>
+        ))}
       </Box>
     </Box>
   )
@@ -71,52 +60,51 @@ const Gender = () => {
 
 export default Gender
 
-const useStyles = makeStyles()(() => {
-  return {
-    genderText: {
-      display: 'flex',
-      justifyContent: 'center',
-      color: '#3B4054',
-      fontFamily: 'Inter',
-      fontWeight: 500,
-      fontSize: '1.125rem',
-      lineHeight: '150%',
-    },
-    containerPosition: {
-      display: 'flex',
-      flexDirection: 'row',
-      margin: 'o auto',
-    },
-    innerContainerPosition: {
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    titleContainer: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      marginBottom: 14,
-    },
-    title: {
-      fontSize: 32,
-      fontWeight: 600,
-      lineHeight: '119%',
-      paddingTop: 0,
-    },
-    subTitle: {
-      fontFamily: 'Inter',
-      fontSize: '1.125rem',
-      color: '#444',
-      fontWeight: 600,
-      lineHeight: 'normal',
-    },
-    tipContainer: {
-      marginTop: 62,
-      marginBottom: 40,
-      textAlign: 'center',
-    },
-    iconsPosition: {
-      paddingLeft: '1rem',
-    },
-  }
-})
+const useStyles = makeStyles()(() => ({
+  genderText: {
+    display: 'flex',
+    justifyContent: 'center',
+    color: '#3B4054',
+    fontFamily: 'Inter',
+    fontWeight: 500,
+    fontSize: '1.125rem',
+    lineHeight: '150%',
+  },
+  containerPosition: {
+    display: 'flex',
+    flexDirection: 'row',
+    margin: '0 auto',
+  },
+  innerContainerPosition: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  titleContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginBottom: 14,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 600,
+    lineHeight: '119%',
+    paddingTop: 0,
+  },
+  subTitle: {
+    fontFamily: 'Inter',
+    fontSize: '1.125rem',
+    color: '#444',
+    fontWeight: 600,
+    lineHeight: 'normal',
+  },
+  tipContainer: {
+    marginTop: 62,
+    marginBottom: 40,
+    textAlign: 'center',
+  },
+  iconsPosition: {
+    paddingLeft: '1rem',
+  },
+}))
