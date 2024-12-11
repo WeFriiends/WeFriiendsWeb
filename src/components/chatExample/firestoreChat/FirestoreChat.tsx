0770c10df429b0979b35953c48eaf4367c7b3bfd
Marina Kotelnikov <oce4ka@gmail.com>
2024-12-11 13:50:03 +0200

    netlify fix

--- src/components/chatExample/firestoreChat/FirestoreChat.tsx
+++ src/components/chatExample/firestoreChat/FirestoreChat.tsx
@@ -62,7 +62,7 @@ const ChatRoom: React.FC<ChatRoomProps> = ({ roomId, userName }) => {
     })
 
     return () => unsubscribe()
-  }, [messagesCollectionRef])
+  })
 
   const handleSendMessage = async () => {
     if (newMessage.trim() !== '') {
