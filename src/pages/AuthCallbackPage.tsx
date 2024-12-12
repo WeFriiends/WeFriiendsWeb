31d12b499e49433520dd68d49c804d80833cd6c0
Svitlana-G <svitlana.golubieva@gmail.com>
2024-12-12 15:05:59 +0100

    Add confirmation message to Auth0

--- src/pages/AuthCallbackPage.tsx
+++ src/pages/AuthCallbackPage.tsx
@@ -1,12 +1,15 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import Logo from 'components/logo/Logo'
+import AccountConfirmationMessage from 'components/userAuth/AccountConfirmationMessage'
 
 const AuthCallbackPage = () => {
   const { error } = useAuth0()
 
   if (error) {
     console.error(error)
-    return <div>Oops... {error.message}</div>
+    if (error.message == 'Please verify your email before logging in.')
+      return <AccountConfirmationMessage />
+    else return <div>Oops... {error.message}</div>
   }
 
   return <Logo />
